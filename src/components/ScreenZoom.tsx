d1e251aaf291a7973306135b99ac05b0bf0e1f02
xMokiii <morlioncolin@gmail.com>
2024-12-25 17:47:37 +0100

    Zoom Fonctionnel fadein Fadeout

--- src/components/ScreenZoom.tsx
+++ src/components/ScreenZoom.tsx
@@ -1,18 +1,17 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import useSmoothScroll from '../hooks/useSmoothScroll';
 import styles from '../styles/ScreenZoom.module.css';
+import useSmoothScroll from '../hooks/useSmoothScroll';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const ScreenZoom: React.FC = () => {
-  useSmoothScroll(); // Enable smooth scrolling with Lenis
+  useSmoothScroll();
+
   const containerRef = useRef<HTMLDivElement>(null);
   const screenRef = useRef<HTMLDivElement>(null);
-  const titleRef = useRef<HTMLHeadingElement>(null);
-  const [isHidden, setIsHidden] = useState(false); // Toggle for the title
-  const [isDivVisible, setIsDivVisible] = useState(false); // Toggle for the appearing div
+  const [isHidden, setIsHidden] = useState(false);
 
   useEffect(() => {
     const container = containerRef.current;
@@ -26,11 +25,7 @@ const ScreenZoom: React.FC = () => {
           end: '+=3500',
           scrub: true,
           pin: container,
-          onUpdate: (self) => {
-            const progress2 = self.progress; // Progress of the scroll
-            setIsHidden(progress2 > 0.15); // Hide the title when progress2 > 0.15
-            setIsDivVisible(progress2 >= 1); // Show the div when zoom is maximum
-          },
+          onUpdate: ({ progress }) => setIsHidden(progress > 0.35),
         },
       })
         .to(screen, {
@@ -39,7 +34,7 @@ const ScreenZoom: React.FC = () => {
           ease: 'power2.inOut',
         })
         .to(screen, {
-          opacity: 0, // Decrease opacity after the zoom ends
+          opacity: 0,
           ease: 'power1.out',
           duration: 0.5,
         });
@@ -48,17 +43,15 @@ const ScreenZoom: React.FC = () => {
 
   return (
     <div ref={containerRef} className={styles.container}>
-      <h1 ref={titleRef} className={styles.title} hidden={isHidden}>
+      <h1 className={styles.title} hidden={isHidden}>
         Bienvenue dans votre solution
       </h1>
       <div ref={screenRef} className={styles.screen}>
         <img src="/laptop2.png" alt="Laptop" className={styles.image} />
       </div>
-      {isDivVisible && (
-        <div className={styles.appearingDiv}>
-          <p>Zoom is at its maximum!</p>
-        </div>
-      )}
+      <div className={styles.appearingDiv} hidden={!isHidden}>
+        <p>Zoom is at its maximum!</p>
+      </div>
     </div>
   );
 };
